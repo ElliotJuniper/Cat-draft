f6ef43903bd1d2464806041c6afd9328a1dac9a1
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-21 09:44:14 +0100

    new

---
+++ src/Basket.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// const Basket = (basket) => {
+//   return (
+  
+//   <div className='basket-item'>Basket items</div>
+//   {basket.length === 0 && (
+//     <div className='basket-empty'>Basket Empty</div>
+//   )}
+//   <div>
+//     {basket.map((item) => (
+//       <div key={item.id} className="basket-list">
+
+//     ))}
+//   </div>
+
+//   )}
+
+// export default Basket
